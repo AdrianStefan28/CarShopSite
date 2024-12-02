c501454a9ece64f671abe1b1a8eadfe5896ac2fc
Stefan Adrian Catalin <92256225+AdrianStefan28@users.noreply.github.com>
2024-12-02 14:35:36 +0200

    Refacut la baza de date

--- CarShop/ServiceAuto/obj/Debug/net7.0/ServiceAuto.AssemblyInfo.cs
+++ CarShop/ServiceAuto/obj/Debug/net7.0/ServiceAuto.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceAuto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ff13f2d31ce37d528cefe59e9272bd9d866e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceAuto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceAuto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
